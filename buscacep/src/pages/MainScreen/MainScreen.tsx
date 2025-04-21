ade3f26b7e12551df6ba9940282cf3b25a367991
YuriRamalhoSilva <yuriramalhosilva67@gmail.com>
2025-04-21 17:13:57 -0300

    First commit

---
+++ buscacep/src/pages/MainScreen/MainScreen.tsx
@@ -0,0 +1,14 @@
+function MainScreen () {
+    
+    
+    return (
+        <div>
+            <h1>BuscaCep</h1>
+            <form action="">
+                <input type="number" />
+            </form>
+        </div>
+    )
+}
+
+export default MainScreen
